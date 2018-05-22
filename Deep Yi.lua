e0b0650f5378f3c6b401cbfb371f42e31d22a9b5
Deep <mrhoalu@gmail.com>
2018-05-22 22:45:23 +0700

    Improved menu

--- Deep Yi.lua
+++ Deep Yi.lua
@@ -259,7 +259,7 @@ if not Menu_Begin(self.menu) then return end
 			Menu_End()
         end
 		if Menu_Begin("Auto") then
-			self.menu_Combo_QendDash = Menu_Bool("Auto Q End Dash", self.menu_Combo_QendDash, self.menu)
+			self.menu_Combo_QendDash = Menu_Bool("Auto Q Dasing Enemies", self.menu_Combo_QendDash, self.menu)
             self.Qinterrupt = Menu_Bool("Evade Spells With Q", self.Qinterrupt, self.menu)
             self.QinterruptHP = Menu_SliderInt("Your min HP% to evade with Q", self.QinterruptHP, 0, 100, self.menu)
             self.Wauto = Menu_Bool("Auto W on low HP", self.Wauto, self.menu)
