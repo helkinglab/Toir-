b1543bc4381260b5e2190ac44b6406821782800d
Deep <mrhoalu@gmail.com>
2018-05-26 00:10:29 +0700

    Improved Combo Logic, added Auto QE on dashing enemies

--- Deep Irelia.lua
+++ Deep Irelia.lua
@@ -2,10 +2,15 @@ IncludeFile("Lib\\TOIR_SDK.lua")
 
 Irelia = class()
 
+local ScriptXan = 2.0
+local NameCreat = "Deep"
+
 function OnLoad()
-    if GetChampName(GetMyChamp()) == "Irelia" then
-		Irelia:TopLane()
-	end
+    if myHero.CharName ~= "Irelia" then return end
+    __PrintTextGame("<b><font color=\"#00FF00\">Champion:</font></b> " ..myHero.CharName.. "<b><font color=\"#FF0000\"> The Blade Dancer!</font></b>")
+    __PrintTextGame("<b><font color=\"#00FF00\">Irelia, v</font></b> " ..ScriptXan)
+    __PrintTextGame("<b><font color=\"#00FF00\">By: </font></b> " ..NameCreat)
+	Irelia:TopLane()
 end
 
 function Irelia:TopLane()
@@ -44,6 +49,8 @@ function Irelia:TopLane()
     Callback.Add("UpdateBuff", function(unit, buff, stacks) self:OnUpdateBuff(source, unit, buff, stacks) end)
 	Callback.Add("RemoveBuff", function(unit, buff) self:OnRemoveBuff(unit, buff) end)
 	
+	Irelia:aa()
+	
  __PrintTextGame("<b><font color=\"#cffffff00\">Deep Irelia</font></b> <font color=\"#ffffff\">Loaded. Enjoy The Blade Dancer</font>")
  end 
 
@@ -68,6 +75,12 @@ function Irelia:OnUpdate()
 	if self.Enable_Mod_Skin then
 		ModSkin(self.Set_Skin)
 	end
+	if self.menu_Combo_QendDash then
+		self:autoQtoEndDash()
+	end
+	if self.menu_Combo_EendDash then
+		self:autoEtoEndDash()
+	end
 end 
 
 
@@ -82,6 +95,9 @@ function Irelia:IreliaMenus()
     self.CE = self:MenuBool("Combo E", true)
     self.CR = self:MenuBool("Combo R", true)
     self.CRlow = self:MenuSliderInt("HP Minimum %", 90)
+	
+	self.menu_Combo_QendDash = self:MenuBool("Auto Q End Dash", false)
+	self.menu_Combo_EendDash = self:MenuBool("Auto E End Dash", true)
    
     --Lane
     self.LQ = self:MenuBool("Lane Q", true)
@@ -132,11 +148,13 @@ if not Menu_Begin(self.menu) then return end
 
 		if Menu_Begin("Combo") then
             self.CQ = Menu_Bool("Combo Q", self.CQ, self.menu)
+			self.menu_Combo_QendDash = Menu_Bool("Auto Q Dasing Enemies", self.menu_Combo_QendDash, self.menu)
             self.CQdis = Menu_SliderInt("Combo minimum Q distance", self.CQdis, 0, 625, self.menu)
 			self.CW = Menu_Bool("Combo W", self.CW, self.menu)
             self.CWHP = Menu_SliderInt("Combo min HP% to use W", self.CWHP, 0, 100, self.menu)
             self.CWdis = Menu_SliderInt("Combo max W range", self.CWdis, 0, 500, self.menu)
 			self.CE = Menu_Bool("Combo E", self.CE, self.menu)
+			self.menu_Combo_EendDash = Menu_Bool("Auto E Dasing Enemies", self.menu_Combo_EendDash, self.menu)
             self.CR = Menu_Bool("Combo R", self.CR, self.menu)
             self.CRlow = Menu_SliderInt("Enemy min HP % for Combo R", self.CRlow, 0, 100, self.menu)
 			Menu_End()
@@ -193,6 +211,58 @@ if not Menu_Begin(self.menu) then return end
 		end
 		Menu_End()
 	end
+
+function Irelia:autoQtoEndDash()
+	for i, enemy in pairs(GetEnemyHeroes()) do
+		if enemy ~= nil then
+		    target = GetAIHero(enemy)
+		    if IsValidTarget(target.Addr, self.Q.range) then
+		    --local QPos, QHitChance = HPred:GetPredict(self.HPred_Q_M, target, myHero)
+			  --  local TargetDashing, CanHitDashing, DashPosition = vpred:IsDashing(target, self.Q.delay, self.Q.width, self.Q.speed, myHero, true)	    	
+			    --if IsValidTarget(target.Addr, self.maxGrab) then
+			    	if target.IsDash then
+						--CastSpellToPos(QPos.x, QPos.z, _Q)
+					--end
+			    --end
+			    --if DashPosition ~= nil then
+			    	--if GetDistance(DashPosition) <= self.Q.range then
+			  		--local Collision = CountObjectCollision(0, target.Addr, myHero.x, myHero.z, DashPosition.x, DashPosition.z, self.Q.width, self.Q.range, 65)
+				  		--local Collision = CountCollision(myHero.x, myHero.z, DashPosition.x, DashPosition.z, self.Q.delay, self.Q.width, self.Q.range, self.Q.speed, 0, 5, 5, 5, 5)
+				  		--if Collision == 0 then
+					    	CastSpellTarget(target.Addr, _Q)
+					    end
+					end
+				end
+			end
+			end	
+	
+function Irelia:autoEtoEndDash()
+	for i, enemy in pairs(GetEnemyHeroes()) do
+		if enemy ~= nil then
+		    target = GetAIHero(enemy)
+		    if IsValidTarget(target.Addr, self.E.range) then
+		    --local QPos, QHitChance = HPred:GetPredict(self.HPred_Q_M, target, myHero)
+			    local TargetDashing, CanHitDashing, DashPosition = self.Predc:IsDashing(target, self.E.delay, self.E.width, self.E.speed, myHero, true)	    	
+			    --if IsValidTarget(target.Addr, self.maxGrab) then
+			    	if target.IsDash then
+					    	CastSpellToPos(DashPosition.x, DashPosition.z, _E)
+		DelayAction(function() 
+		        self.E:Cast(myHero.Addr)
+				end, 0.5)
+						--CastSpellToPos(QPos.x, QPos.z, _Q)
+					--end
+			    --end
+			    --if DashPosition ~= nil then
+			    	--if GetDistance(DashPosition) <= self.Q.range then
+			  		--local Collision = CountObjectCollision(0, target.Addr, myHero.x, myHero.z, DashPosition.x, DashPosition.z, self.Q.width, self.Q.range, 65)
+				  		--local Collision = CountCollision(myHero.x, myHero.z, DashPosition.x, DashPosition.z, self.Q.delay, self.Q.width, self.Q.range, self.Q.speed, 0, 5, 5, 5, 5)
+				  		--if Collision == 0 then
+					    	--CastSpellTarget(target.Addr, _Q)
+					    end
+					end
+				end
+			end
+		end
 	
 function Irelia:EnemyMinionsTbl() --SDK Toir+
     GetAllUnitAroundAnObject(myHero.Addr, 2000)
@@ -258,9 +328,10 @@ function Irelia:KillEnemy()
     local RKS = GetTargetSelector(self.R.range)
     Enemy = GetAIHero(RKS)
     if CanCast(_R) and self.KR and RKS ~= 0 and GetDistance(Enemy) < 900 and GetDamage("R", Enemy) > Enemy.HP then
-        local CEPosition, HitChance, Position = self.Predc:GetLineCastPosition(Enemy, self.R.delay, self.R.width, self.R.range, self.R.speed, myHero, false)
-		if HitChance >= 3 then
-			CastSpellToPos(CEPosition.x, CEPosition.z, _R)
+	 			target = GetAIHero(Enemy)
+        			local CastPosition, HitChance, Position = self:GetRLinePreCore(target)
+			if HitChance >= 6 then
+        		CastSpellToPos(CastPosition.x, CastPosition.z, _R)
         end
     end  
 end 
@@ -377,6 +448,17 @@ function Irelia:GetELinePreCore(target)
 	return nil , 0 , nil
 end
 
+function Irelia:GetRLinePreCore(target)
+	local castPosX, castPosZ, unitPosX, unitPosZ, hitChance, _aoeTargetsHitCount = GetPredictionCore(target.Addr, 0, self.R.delay, self.R.width, self.R.range, self.R.speed, myHero.x, myHero.z, false, true, 1, 3, 5, 5, 5, 5)
+	if target ~= nil then
+		 CastPosition = Vector(castPosX, target.y, castPosZ)
+		 HitChance = hitChance
+		 Position = Vector(unitPosX, target.y, unitPosZ)
+		 return CastPosition, HitChance, Position
+	end
+	return nil , 0 , nil
+end
+
 function Irelia:CastE()
 	local TargetE = GetTargetSelector(self.E.range - 150, 1)
 	if TargetE ~= 0 then
@@ -424,10 +506,11 @@ end
 function Irelia:CastR()
     local Rcombo = GetTargetSelector(1000)
     Enemy = GetAIHero(Rcombo)
-    if CanCast(_R) and self.CR and Rcombo ~= 0 and GetDistance(Enemy) < 800 and GetPercentHP(Enemy.Addr) < self.CRlow then
-        local CEPosition, HitChance, Position = self.Predc:GetLineCastPosition(Enemy, self.R.delay, self.R.width, self.R.range, self.R.speed, myHero, false)
-		if HitChance >= 3 then
-			CastSpellToPos(CEPosition.x, CEPosition.z, _R)
+    if CanCast(_R) and self.CR and Rcombo ~= 0 and GetDistance(Enemy) < 800 and GetPercentHP(Enemy.Addr) <= self.CRlow then
+	 			target = GetAIHero(Enemy)
+        			local CastPosition, HitChance, Position = self:GetRLinePreCore(target)
+			if HitChance >= 5 then
+        		CastSpellToPos(CastPosition.x, CastPosition.z, _R)
         end
     end   
 end 
@@ -534,3 +617,72 @@ function Irelia:OnTick()
         self:CastR()
     end
 end 
+
+function Irelia:aa()
+	self.listEndDash =
+	{
+		{Name = "ZoeR", RangeMin = 570, Range = 570, Type = 1, Duration = 0.25}, --CHUAN
+		{Name = "MaokaiW", RangeMin = 0, Range = math.huge, Type = 1, Duration = 0.25}, --CHUAN
+		{Name = "CamilleE", RangeMin = 0, Range = math.huge, Type = 1, Duration = 1.25}, --MaokaiW
+		{Name = "BlindMonkQTwo", RangeMin = 0, Range = math.huge, Type = 1, Duration = 0.25}, --MaokaiW
+		{Name = "BlindMonkWOne", RangeMin = 0, Range = math.huge, Type = 1, Duration = 0.25}, --MaokaiW
+		{Name = "NocturneParanoia2", RangeMin = 0, Range = math.huge, Type = 1, Duration = 0.25}, --MaokaiW
+		{Name = "XinZhaoE", RangeMin = 0, Range = 100, Type = 2, Duration = 0.25}, --CHUAN
+		{Name = "PantheonW", RangeMin = 0, Range = 200, Type = 2, Duration = 0.25}, --CHUAN
+		{Name = "AkaliShadowDance", RangeMin = 0, Range = - 100, Type = 2, Duration = 0.25}, --CHUAN
+		{Name = "AkaliSmokeBomb", RangeMin = 0, Range = 250, Type = 1, Duration = 0.25}, --CHUAN
+		{Name = "Headbutt", RangeMin = 0, Range = math.huge, Type = 1, Duration = 0.25}, --CHUAN
+		{Name = "BraumW", RangeMin = 0, Range = - 140, Type = 2, Duration = 0.25}, --CHUAN
+		{Name = "DianaTeleport", RangeMin = 0, Range = 80, Type = 2, Duration = 0.25}, --50% CHUAN
+		{Name = "JaxLeapStrike", RangeMin = 0, Range = math.huge, Type = 1, Duration = 0.25}, --CHUAN
+		{Name = "MonkeyKingNimbus", RangeMin = 0, Range = math.huge, Type = 1, Duration = 0.25}, --CHUAN
+		{Name = "PoppyE", RangeMin = 0, Range = math.huge, Type = 1, Duration = 0.25}, --MaokaiW
+		{Name = "IreliaGatotsu", RangeMin = 0, Range = math.huge, Type = 1, Duration = 0.25}, --CHUAN
+		{Name = "UFSlash", RangeMin = 0, Range = math.huge, Type = 1, Duration = 0.25}, --CHUAN MalphiteR
+		{Name = "LucianE", RangeMin = 200, Range = 430, Type = 1, Duration = 0.25}, --CHUAN
+		{Name = "EzrealArcaneShift", RangeMin = 0, Range = 470, Type = 1, Duration = 0.25}, --CHUAN
+		{Name = "TristanaW", RangeMin = 0, Range = 900, Type = 1, Duration = 0.25}, --CHUAN
+		{Name = "SummonerFlash", RangeMin = 0, Range = 400, Type = 1, Duration = 0.25}, --CHUAN
+		{Name = "AhriTumble", RangeMin = 0, Range = 500, Type = 1, Duration = 0.25}, --CHUAN
+		{Name = "CarpetBomb", RangeMin = 300, Range = 600, Type = 1, Duration = 0.25}, --CHUAN
+		{Name = "FioraQ", RangeMin = 0, Range = 400, Type = 1, Duration = 0.25}, --CHUAN
+		{Name = "KindredQ", RangeMin = 0, Range = 300, Type = 1, Duration = 0.25}, --CHUAn
+		{Name = "RiftWalk", RangeMin = 0, Range = 500, Type = 1, Duration = 0.25}, --CHUAN
+		{Name = "FizzETwo", RangeMin = 0, Range = 300, Type = 1, Duration = 0.25}, --CHUAN
+		{Name = "FizzE", RangeMin = 0, Range = 400, Type = 1, Duration = 0.25}, --CHUAN
+		{Name = "CamilleEDash2", RangeMin = 0, Range = 400, Type = 1, Duration = 0.25}, --50% CHUAN
+		{Name = "AatroxQ", RangeMin = 0, Range = 650, Type = 1, Duration = 0.5}, --CHUAN
+		{Name = "RakanW", RangeMin = 0, Range = 650, Type = 1, Duration = 0.25}, --CHUAN
+		{Name = "QuinnE", RangeMin = 0, Range = 600, Type = 1, Duration = 0.25}, --CHUAN
+		{Name = "JarvanIVDemacianStandard", RangeMin = 0, Range = 850, Type = 1, Duration = 0.25}, --Ezreal E
+		{Name = "ShyvanaTransformLeap", RangeMin = 0, Range = 1000, Type = 1, Duration = 0.25}, --Ezreal E
+		{Name = "ShenE", RangeMin = 300, Range = 600, Type = 1, Duration = 0.5}, --CHUAN
+		{Name = "Deceive", RangeMin = 0, Range = 400, Type = 1, Duration = 0.25}, --CHUAN
+		{Name = "SejuaniQ", RangeMin = 0, Range = 650, Type = 1, Duration = 0.25}, --Ezreal E
+		{Name = "KhazixE", RangeMin = 0, Range = 700, Type = 1, Duration = 0.25}, --CHUAN
+		{Name = "KhazixELong", RangeMin = 0, Range = 900, Type = 1, Duration = 0.25}, --CHUAN
+		{Name = "TryndamereE", RangeMin = 0, Range = 650, Type = 1, Duration = 0.25}, --CHUAN
+		{Name = "LeblancW", RangeMin = 0, Range = 600, Type = 1, Duration = 0.25}, --Ezreal E
+		{Name = "GalioE", RangeMin = 0, Range = 625, Type = 1, Duration = 0.5}, --Ezreal E
+		{Name = "ZacE", RangeMin = 0, Range = 1200, Type = 1, Duration = 1}, --Ezreal E
+		{Name = "ViQ", RangeMin = 0, Range = 720, Type = 1, Duration = 0.25}, --Ezreal E
+		{Name = "EkkoEAttack", RangeMin = 0, Range = 150, Type = 2, Duration = 0.25}, --CHUAN
+		{Name = "TalonQ", RangeMin = 0, Range = 120, Type = 2, Duration = 0.25}, --CHUAN
+		{Name = "EkkoE", RangeMin = 350, Range = 350, Type = 1, Duration = 0.25}, --CHUAN
+		{Name = "FizzQ", RangeMin = 550, Range = 600, Type = 1, Duration = 0.25}, --50% CHUAN
+		{Name = "GragasE", RangeMin = 700, Range = 600, Type = 1, Duration = 0.25}, --Ezreal E
+		{Name = "GravesMove", RangeMin = 280, Range = 370, Type = 1, Duration = 0.25}, --CHUAN
+		{Name = "OrnnE", RangeMin = 650, Range = 650, Type = 1, Duration = 0.75}, --CHUAN
+		{Name = "Pounce", RangeMin = 370, Range = 370, Type = 1, Duration = 0.25}, --CHUAN
+		{Name = "RivenFeint", RangeMin = 250, Range = 250, Type = 1, Duration = 0.25}, --CHUAN
+		{Name = "KaynQ", RangeMin = 350, Range = 350, Type = 1, Duration = 0.25}, --CHUAN
+		{Name = "RenektonSliceAndDice", RangeMin = 450, Range = 450, Type = 1, Duration = 0.25}, --CHUAN
+		{Name = "RenektonDice", RangeMin = 450, Range = 450, Type = 1, Duration = 0.25}, --CHUAN
+		{Name = "VayneTumble", RangeMin = 300, Range = 300, Type = 1, Duration = 0.25}, --CHUAN
+		{Name = "UrgotE", RangeMin = 470, Range = 470, Type = 3, Duration = 0.25}, --Ezreal E
+		{Name = "JarvanIVDragonStrike", RangeMin = 850, Range = 850, Type = 1, Duration = 0.25}, --Ezreal E
+		{Name = "WarwickR", RangeMin = 1000, Range = 1000, Type = 1, Duration = 1}, --CHUAN
+		{Name = "ZiggsDashWrapper", RangeMin = 480, Range = 480, Type = 1, Duration = 0.25}, --CHUAN
+		{Name = "CaitlynEntrapment", RangeMin = -380, Range = -380, Type = 1, Duration = 0.25}, --CHUAN
+	}
+end
