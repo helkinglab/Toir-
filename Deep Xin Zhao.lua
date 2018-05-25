8a9bd4ea88a2c2d940fb6b558bd8d4dc4c1a841b
Deep <mrhoalu@gmail.com>
2018-05-25 19:15:37 +0700

    Improve combo, jungle and added a new auto E option

--- Deep Xin Zhao.lua
+++ Deep Xin Zhao.lua
@@ -2,10 +2,15 @@ IncludeFile("Lib\\TOIR_SDK.lua")
 
 XinZhao = class()
 
+local ScriptXan = 2.0
+local NameCreat = "Deep"
+
 function OnLoad()
-    if GetChampName(GetMyChamp()) == "XinZhao" then
-		XinZhao:TopLane()
-	end
+    if myHero.CharName ~= "XinZhao" then return end
+    __PrintTextGame("<b><font color=\"#00FF00\">Champion:</font></b> " ..myHero.CharName.. "<b><font color=\"#FF0000\"> The Seneschal Of Demacia!</font></b>")
+    __PrintTextGame("<b><font color=\"#00FF00\">Xin Zhao, v</font></b> " ..ScriptXan)
+    __PrintTextGame("<b><font color=\"#00FF00\">By: </font></b> " ..NameCreat)
+	XinZhao:TopLane()
 end
 
 function XinZhao:TopLane()
@@ -21,6 +26,8 @@ function XinZhao:TopLane()
     self.menu_ts = TargetSelector(1750, 0, myHero, true, true, true)
     self.Predc = VPrediction(true)
 
+	self.posEndDash = Vector(0, 0, 0)	
+	
     self:XinZhaoMenus()
 
 		--Spells	
@@ -33,6 +40,8 @@ function XinZhao:TopLane()
 	self.W:SetSkillShot(0.30, 1600, 100, false)
     self.E:SetTargetted()
     self.R:SetActive()
+	
+	XinZhao:aa()
 	
     self.listSpellInterrup = {
     ["CaitlynAceintheHole"] = true,
@@ -63,7 +72,7 @@ function XinZhao:TopLane()
 	Callback.Add("AfterAttack", function(...) self:OnAfterAttack(...) end)
 	Callback.Add("ProcessSpell", function(...) self:OnProcessSpell(...) end)
 	
- __PrintTextGame("<b><font color=\"#cffffff00\">Deep XinZhao</font></b> <font color=\"#ffffff\">Loaded. Enjoy</font>")
+ __PrintTextGame("<b><font color=\"#cffffff00\">Deep XinZhao</font></b> <font color=\"#ffffff\">Loaded. Enjoy The Seneschal Of Demacia</font>")
  end 
  
 --SDK {{Toir+}}
@@ -87,7 +96,34 @@ function XinZhao:OnUpdate()
 	if self.Enable_Mod_Skin then
 		ModSkin(self.Set_Skin)
 	end
+	if self.menu_Combo_QendDash then
+		self:autoQtoEndDash()
+	end
 end 
+
+function XinZhao:autoQtoEndDash()
+	for i, enemy in pairs(GetEnemyHeroes()) do
+		if enemy ~= nil then
+		    target = GetAIHero(enemy)
+		    if IsValidTarget(target.Addr, self.E.range) then
+		    --local QPos, QHitChance = HPred:GetPredict(self.HPred_Q_M, target, myHero)
+			  --  local TargetDashing, CanHitDashing, DashPosition = vpred:IsDashing(target, self.Q.delay, self.Q.width, self.Q.speed, myHero, true)	    	
+			    --if IsValidTarget(target.Addr, self.maxGrab) then
+			    	if target.IsDash then
+						--CastSpellToPos(QPos.x, QPos.z, _Q)
+					--end
+			    --end
+			    --if DashPosition ~= nil then
+			    	--if GetDistance(DashPosition) <= self.Q.range then
+			  		--local Collision = CountObjectCollision(0, target.Addr, myHero.x, myHero.z, DashPosition.x, DashPosition.z, self.Q.width, self.Q.range, 65)
+				  		--local Collision = CountCollision(myHero.x, myHero.z, DashPosition.x, DashPosition.z, self.Q.delay, self.Q.width, self.Q.range, self.Q.speed, 0, 5, 5, 5, 5)
+				  		--if Collision == 0 then
+					    	CastSpellTarget(target.Addr, _E)
+					    end
+					end
+				end
+			end
+			end
  
   function XinZhao:XinZhaoMenus()
    self.menu = "Deep XinZhao"
@@ -95,12 +131,13 @@ end
    self.CQ = self:MenuBool("Combo Q", true)
    self.CW = self:MenuBool("Combo W", true)
    self.CE = self:MenuBool("Combo E", true)
+	self.menu_Combo_QendDash = self:MenuBool("Auto Q End Dash", false)
    self.CWdis = self:MenuSliderInt("Combo W distance", 250)
    self.CWhar = self:MenuBool("Harass W", true)
    self.CWHarassdis = self:MenuSliderInt("Harass W distance", 900)
    self.CEdis = self:MenuSliderInt("Combo minimum E Distance", 350)
    
-	--Auto Interrupt
+	--Auto
 	self.Rinterrupt = self:MenuBool("Interrupt Spells With R", true)
    
    --Add R
@@ -140,6 +177,7 @@ if not Menu_Begin(self.menu) then return end
        self.CW = Menu_Bool("Combo W", self.CW, self.menu)
        self.CWdis = Menu_SliderInt("Combo W distance", self.CWdis, 100, 900, self.menu)
        self.CE = Menu_Bool("Combo E", self.CE, self.menu)
+		self.menu_Combo_QendDash = Menu_Bool("Auto E Dasing Enemies", self.menu_Combo_QendDash, self.menu)
        self.CEdis = Menu_SliderInt("Combo minimum E Distance", self.CEdis, 0, 500, self.menu)
        self.CWhar = Menu_Bool("Harass W", self.CWhar, self.menu)
        self.CWHarassdis = Menu_SliderInt("Harass W distance", self.CWHarassdis, 100, 900, self.menu)
@@ -230,7 +268,7 @@ function XinZhao:OnAfterAttack(unit, target)
 	  if GetKeyPress(self.LaneClear) > 0
 	  and CanCast(_Q)
 	  and self.JQ
-	  and GetPercentMP(myHero.Addr) > self.JQMana
+	  and GetPercentMP(myHero.Addr) >= self.JQMana
 	  then
 		if jungle ~= nil and GetDistance(jungle) < self.Q.range then
 			self.Q:Cast(myHero.Addr)
@@ -254,9 +292,10 @@ function XinZhao:Wcom()
     local UseW = GetTargetSelector(1000)
     if UseW then Enemy = GetAIHero(UseW) end
     if CanCast(_W) and self.CW and IsValidTarget(Enemy, self.CWdis) then 
-        local CEPosition, HitChance, Position = self.Predc:GetLineCastPosition(Enemy, self.W.delay, self.W.width, self.W.range, self.W.speed, myHero, false)
-		if HitChance >= 2 then
-			CastSpellToPos(CEPosition.x, CEPosition.z, _W)
+        target = GetAIHero(Enemy)
+        			local CastPosition, HitChance, Position = self:GetWLinePreCore(target)
+			if HitChance >= 6 then
+        		CastSpellToPos(CastPosition.x, CastPosition.z, _W)
         end
     end 
 end
@@ -265,9 +304,10 @@ function XinZhao:WHarass()
     local UseW = GetTargetSelector(1000)
     if UseW then Enemy = GetAIHero(UseW) end
     if CanCast(_W) and self.CWhar and IsValidTarget(Enemy, self.CWHarassdis) then 
-        local CEPosition, HitChance, Position = self.Predc:GetLineCastPosition(Enemy, self.W.delay, self.W.width, self.W.range, self.W.speed, myHero, false)
-		if HitChance >= 2 then
-			CastSpellToPos(CEPosition.x, CEPosition.z, _W)
+        target = GetAIHero(Enemy)
+        			local CastPosition, HitChance, Position = self:GetWLinePreCore(target)
+			if HitChance >= 6 then
+        		CastSpellToPos(CastPosition.x, CastPosition.z, _W)
         end
     end 
 end
@@ -302,13 +342,14 @@ function XinZhao:JungleClear()
     GetAllUnitAroundAnObject(myHero.Addr, 2000)
     local result = {}
     for i, minions in pairs(pUnit) do
-        if minions ~= 0 and not IsDead(minions) and not IsInFog(minions) and (GetType(minions) == 3 or GetType(minions) == 2 or GetType(minions) == 1) then
+        if minions ~= 0 and not IsDead(minion) and not IsInFog(minions) and (GetType(minions) == 3 or GetType(minions) == 2 or GetType(minions) == 1) then
             table.insert(result, minions)
         end
     end
 
     return result
 end 
+
 
 function XinZhao:GetWLinePreCore(target)
 	local castPosX, castPosZ, unitPosX, unitPosZ, hitChance, _aoeTargetsHitCount = GetPredictionCore(target.Addr, 0, self.W.delay, self.W.width, self.W.range, self.W.speed, myHero.x, myHero.z, false, true, 1, 3, 5, 5, 5, 5)
@@ -368,7 +409,8 @@ function XinZhao:CountEnemiesInRange(pos, range)
     end
     return n
 end
-function XinZhao:FarmJungle()
+
+function XinZhao:JungleW()
 	if CanCast(_W) and self.JW and GetPercentMP(myHero.Addr) > self.JWMana and (GetType(GetTargetOrb()) == 3) then
 		if (GetObjName(GetTargetOrb()) ~= "PlantSatchel" and GetObjName(GetTargetOrb()) ~= "PlantHealth" and GetObjName(GetTargetOrb()) ~= "PlantVision") then
 			target = GetUnit(GetTargetOrb())
@@ -376,12 +418,38 @@ function XinZhao:FarmJungle()
 			CastSpellToPos(targetPos.x, targetPos.z, _W)
 		end
     end
-	if CanCast(_E) and self.JE and GetPercentMP(myHero.Addr) > self.JEMana and (GetType(GetTargetOrb()) == 3) then
-		if (GetObjName(GetTargetOrb()) ~= "PlantSatchel" and GetObjName(GetTargetOrb()) ~= "PlantHealth" and GetObjName(GetTargetOrb()) ~= "PlantVision") then
-			target = GetUnit(GetTargetOrb())
-			CastSpellTarget(target.Addr, _E)
-		end
 	end
+
+function XinZhao:JungleMinionsKILL() --SDK Toir+
+    GetAllUnitAroundAnObject(myHero.Addr, 2000)
+    local result = {}
+    for i, obj in pairs(pUnit) do
+        if obj ~= 0  then
+            local jungle = GetUnit(obj)
+            if not IsDead(jungle.Addr)
+			and not	IsInFog(jungle.Addr)
+			and GetType(jungle.Addr) == 3
+			then
+			if (GetObjName(jungle.Addr) ~= "PlantSatchel" and GetObjName(jungle.Addr) ~= "PlantHealth" and GetObjName(jungle.Addr) ~= "PlantVision") then
+                table.insert(result, jungle)
+            end
+			end
+        end
+    end
+    return result
+end	
+	
+function XinZhao:JungleE()
+    for i ,jungle in pairs(self:JungleMinionsKILL()) do
+        if jungle ~= 0 then
+       if GetPercentMP(myHero.Addr) >= self.JEMana
+	   and IsValidTarget(jungle.Addr, self.E.range)
+	   and self.JE
+	   then
+	   CastSpellTarget(jungle.Addr, _E)
+       end 
+    end 
+end
 end
 
 function XinZhao:OnTick()
@@ -392,7 +460,8 @@ function XinZhao:OnTick()
     if GetKeyPress(self.LaneClear) > 0 then
 		local target = self.menu_ts:GetTarget()	
         self:ToTurrent()
-		self:FarmJungle()
+		self:JungleW()
+		self:JungleE()
     end
 	
     if GetKeyPress(self.Harass) > 0 then
@@ -405,3 +474,72 @@ function XinZhao:OnTick()
 		self:RComp()
     end
 end 
+
+function XinZhao:aa()
+	self.listEndDash =
+	{
+		{Name = "ZoeR", RangeMin = 570, Range = 570, Type = 1, Duration = 0.25}, --CHUAN
+		{Name = "MaokaiW", RangeMin = 0, Range = math.huge, Type = 1, Duration = 0.25}, --CHUAN
+		{Name = "CamilleE", RangeMin = 0, Range = math.huge, Type = 1, Duration = 1.25}, --MaokaiW
+		--{Name = "BlindMonkQTwo", RangeMin = 0, Range = math.huge, Type = 1, Duration = 0.25}, --MaokaiW
+		{Name = "BlindMonkWOne", RangeMin = 0, Range = math.huge, Type = 1, Duration = 0.25}, --MaokaiW
+		{Name = "NocturneParanoia2", RangeMin = 0, Range = math.huge, Type = 1, Duration = 0.25}, --MaokaiW
+		{Name = "XinZhaoE", RangeMin = 0, Range = 100, Type = 2, Duration = 0.25}, --CHUAN
+		{Name = "PantheonW", RangeMin = 0, Range = 200, Type = 2, Duration = 0.25}, --CHUAN
+		{Name = "AkaliShadowDance", RangeMin = 0, Range = - 100, Type = 2, Duration = 0.25}, --CHUAN
+		{Name = "AkaliSmokeBomb", RangeMin = 0, Range = 250, Type = 1, Duration = 0.25}, --CHUAN
+		{Name = "Headbutt", RangeMin = 0, Range = math.huge, Type = 1, Duration = 0.25}, --CHUAN
+		{Name = "BraumW", RangeMin = 0, Range = - 140, Type = 2, Duration = 0.25}, --CHUAN
+		{Name = "DianaTeleport", RangeMin = 0, Range = 80, Type = 2, Duration = 0.25}, --50% CHUAN
+		{Name = "JaxLeapStrike", RangeMin = 0, Range = math.huge, Type = 1, Duration = 0.25}, --CHUAN
+		{Name = "MonkeyKingNimbus", RangeMin = 0, Range = math.huge, Type = 1, Duration = 0.25}, --CHUAN
+		{Name = "PoppyE", RangeMin = 0, Range = math.huge, Type = 1, Duration = 0.25}, --MaokaiW
+		{Name = "IreliaGatotsu", RangeMin = 0, Range = math.huge, Type = 1, Duration = 0.25}, --CHUAN
+		{Name = "UFSlash", RangeMin = 0, Range = math.huge, Type = 1, Duration = 0.25}, --CHUAN MalphiteR
+		{Name = "LucianE", RangeMin = 200, Range = 430, Type = 1, Duration = 0.25}, --CHUAN
+		{Name = "EzrealArcaneShift", RangeMin = 0, Range = 470, Type = 1, Duration = 0.25}, --CHUAN
+		{Name = "TristanaW", RangeMin = 0, Range = 900, Type = 1, Duration = 0.25}, --CHUAN
+		{Name = "SummonerFlash", RangeMin = 0, Range = 400, Type = 1, Duration = 0.25}, --CHUAN
+		{Name = "AhriTumble", RangeMin = 0, Range = 500, Type = 1, Duration = 0.25}, --CHUAN
+		{Name = "CarpetBomb", RangeMin = 300, Range = 600, Type = 1, Duration = 0.25}, --CHUAN
+		{Name = "FioraQ", RangeMin = 0, Range = 400, Type = 1, Duration = 0.25}, --CHUAN
+		{Name = "KindredQ", RangeMin = 0, Range = 300, Type = 1, Duration = 0.25}, --CHUAn
+		{Name = "RiftWalk", RangeMin = 0, Range = 500, Type = 1, Duration = 0.25}, --CHUAN
+		{Name = "FizzETwo", RangeMin = 0, Range = 300, Type = 1, Duration = 0.25}, --CHUAN
+		{Name = "FizzE", RangeMin = 0, Range = 400, Type = 1, Duration = 0.25}, --CHUAN
+		{Name = "CamilleEDash2", RangeMin = 0, Range = 400, Type = 1, Duration = 0.25}, --50% CHUAN
+		{Name = "AatroxQ", RangeMin = 0, Range = 650, Type = 1, Duration = 0.5}, --CHUAN
+		{Name = "RakanW", RangeMin = 0, Range = 650, Type = 1, Duration = 0.25}, --CHUAN
+		{Name = "QuinnE", RangeMin = 0, Range = 600, Type = 1, Duration = 0.25}, --CHUAN
+		{Name = "JarvanIVDemacianStandard", RangeMin = 0, Range = 850, Type = 1, Duration = 0.25}, --Ezreal E
+		{Name = "ShyvanaTransformLeap", RangeMin = 0, Range = 1000, Type = 1, Duration = 0.25}, --Ezreal E
+		{Name = "ShenE", RangeMin = 300, Range = 600, Type = 1, Duration = 0.5}, --CHUAN
+		{Name = "Deceive", RangeMin = 0, Range = 400, Type = 1, Duration = 0.25}, --CHUAN
+		{Name = "SejuaniQ", RangeMin = 0, Range = 650, Type = 1, Duration = 0.25}, --Ezreal E
+		{Name = "KhazixE", RangeMin = 0, Range = 700, Type = 1, Duration = 0.25}, --CHUAN
+		{Name = "KhazixELong", RangeMin = 0, Range = 900, Type = 1, Duration = 0.25}, --CHUAN
+		{Name = "TryndamereE", RangeMin = 0, Range = 650, Type = 1, Duration = 0.25}, --CHUAN
+		{Name = "LeblancW", RangeMin = 0, Range = 600, Type = 1, Duration = 0.25}, --Ezreal E
+		{Name = "GalioE", RangeMin = 0, Range = 625, Type = 1, Duration = 0.5}, --Ezreal E
+		{Name = "ZacE", RangeMin = 0, Range = 1200, Type = 1, Duration = 1}, --Ezreal E
+		--{Name = "ViQ", RangeMin = 0, Range = 720, Type = 1, Duration = 0.25}, --Ezreal E
+		{Name = "EkkoEAttack", RangeMin = 0, Range = 150, Type = 2, Duration = 0.25}, --CHUAN
+		{Name = "TalonQ", RangeMin = 0, Range = 120, Type = 2, Duration = 0.25}, --CHUAN
+		{Name = "EkkoE", RangeMin = 350, Range = 350, Type = 1, Duration = 0.25}, --CHUAN
+		{Name = "FizzQ", RangeMin = 550, Range = 600, Type = 1, Duration = 0.25}, --50% CHUAN
+		{Name = "GragasE", RangeMin = 700, Range = 600, Type = 1, Duration = 0.25}, --Ezreal E
+		{Name = "GravesMove", RangeMin = 280, Range = 370, Type = 1, Duration = 0.25}, --CHUAN
+		{Name = "OrnnE", RangeMin = 650, Range = 650, Type = 1, Duration = 0.75}, --CHUAN
+		{Name = "Pounce", RangeMin = 370, Range = 370, Type = 1, Duration = 0.25}, --CHUAN
+		{Name = "RivenFeint", RangeMin = 250, Range = 250, Type = 1, Duration = 0.25}, --CHUAN
+		{Name = "KaynQ", RangeMin = 350, Range = 350, Type = 1, Duration = 0.25}, --CHUAN
+		{Name = "RenektonSliceAndDice", RangeMin = 450, Range = 450, Type = 1, Duration = 0.25}, --CHUAN
+		{Name = "RenektonDice", RangeMin = 450, Range = 450, Type = 1, Duration = 0.25}, --CHUAN
+		{Name = "VayneTumble", RangeMin = 300, Range = 300, Type = 1, Duration = 0.25}, --CHUAN
+		{Name = "UrgotE", RangeMin = 470, Range = 470, Type = 3, Duration = 0.25}, --Ezreal E
+		{Name = "JarvanIVDragonStrike", RangeMin = 850, Range = 850, Type = 1, Duration = 0.25}, --Ezreal E
+		{Name = "WarwickR", RangeMin = 1000, Range = 1000, Type = 1, Duration = 1}, --CHUAN
+		{Name = "YasuoDashWrapper", RangeMin = 480, Range = 480, Type = 1, Duration = 0.25}, --CHUAN
+		{Name = "CaitlynEntrapment", RangeMin = -380, Range = -380, Type = 1, Duration = 0.25}, --CHUAN
+	}
+end
